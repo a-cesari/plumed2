38590c698e15aade9e0b2d38af660a9945e9ee97
Andrea Cesari <andreacesari90@gmail.com>
2018-04-20 17:35:37 +0200

    Removed close buttons; Menus are now closed by only one open/close button; Better top menu scrolling on opening

--- user-doc/custom.js
+++ user-doc/custom.js
@@ -1,42 +1,43 @@
-/*
 $( document ).ready(function() {
-$("#side-nav").addClass("w3-sidebar w3-bar-block plmd-sidebar");
-//$("#side-nav").css("display", "none");
-$("#doc-content").before("<button class=\"side-nav-btn w3-button w3-xlarge\" onclick=\"w3_open()\">☰</button>");
-$("#side-nav").prepend("<button onclick=\"w3_close()\" class=\"side-nav-close-btn w3-bar-item w3-button w3-large\">Close &times;</button>");
-});
-function w3_open() {
-    document.getElementById("side-nav").style.width = "100%";
-    document.getElementById("side-nav").style.display = "block";
-}
-function w3_close() {
-    document.getElementById("side-nav").style.display = "none";
-}
-*/
-$( document ).ready(function() {
+//Add necessary class to the side bar to only show on mobile devices
 $("#side-nav").addClass("w3-sidebar w3-bar-block .w3-animate-left-side-nav desktop-only");
-$("#main-nav").addClass("desktop-only");
-$("#main-menu").append("<button onclick=\"w3_close2()\" class=\"w3-button w3-large mobile-only\">Close &times;</button>");
+//Add plumed logo
 $("#nav-tree").prepend("<div id=\"side-bar-logo\" class=\"mobile-only\"><img src=\"logo.png\" /></div>");
-//$("#side-nav").prepend("<button onclick=\"w3_close()\" class=\"w3-bar-item w3-button w3-large mobile-only\">Close &times;</button>");
-//$("body").prepend("<button class=\"side-nav-btn w3-button w3-xlarge mobile-only\" onclick=\"w3_open()\">☰</button>");
-//$(".headertitle").after("<button class=\"side-nav-btn w3-button w3-xlarge mobile-only\" onclick=\"w3_open2()\">☰</button>");
-
 
 });
-function w3_open() {
-    $("#side-nav").removeClass("desktop-only").addClass("side-nav-open-mobile");
-		document.getElementById("myOverlay").style.display = "block";
+
+//Open close the main menu (top one) 
+function main_menu_trigger(){
+		panel=document.getElementById("main-menu");
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
 }
-function w3_open2() {
-    $("#main-nav").removeClass("desktop-only").addClass("main-nav-open-mobile");
+//Close main menu.
+//If main menu is open when trying to open the left side bar, main menu is closed by calling this function
+function main_menu_close(){
+		panel=document.getElementById("main-menu");
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } 
 }
-function w3_close2() {
-    $("#main-nav").removeClass("main-nav-open-mobile").addClass("desktop-only");
+//Trigger main menu open/close when clicking on #main-nav-button
+$( "#main-nav-button" ).click(function() {
+	main_menu_trigger();	
+});
+
+//Open sidebar
+function sidenav_open() {
+    main_menu_close();
+		$("#side-nav").addClass("is-open");
+		$("#myOverlay").addClass("is-active");
+}
+//Close side bar
+function sidenav_close() {
+    //document.getElementById("side-nav").style.display = "none";
+    $("#side-nav").removeClass("is-open"); //aggiungere anche che ritorna a larghezza desktop
+		$("#myOverlay").removeClass("is-active");
 }
 
-function w3_close() {
-    document.getElementById("side-nav").style.display = "none";
-    $("#side-nav").removeClass("side-nav-open-mobile").addClass("desktop-only"); //aggiungere anche che ritorna a larghezza desktop
-		document.getElementById("myOverlay").style.display = "none";
-}
